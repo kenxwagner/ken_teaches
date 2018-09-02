69480da11bcb0a3ce8a6f8f8f53a0527f983a53e
Ken Wagner <ken_xwagner@hotmail.com>
2018-09-01 23:01:12 -0700

    cocatenate in c#
    Needs video

---
+++ Interview Help/Strings/C#/sCocatenate1.cs
@@ -0,0 +1,11 @@
+using System;
+ 
+class Program {
+    static void Main(string[] args) {
+        var s = "What a wonderful world";
+        Console.Write(s);
+        Console.WriteLine(" literal");
+        var s2 = s + " literal";
+        Console.WriteLine(s2);
+    }
+}
