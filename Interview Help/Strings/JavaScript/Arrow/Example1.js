1ae5d570f064128f6aeaca453b1bff3a6b25ba37
Ken Wagner <ken_xwagner@hotmail.com>
2018-09-04 23:06:38 -0700

    arrow play

---
+++ Interview Help/Strings/JavaScript/Arrow/Example1.js
@@ -0,0 +1,13 @@
+var cars = [
+	'Civic',
+	'Corolla',
+	'A4',
+	'Camry',
+	'Impala',
+	'Sienna',
+	'Forester',
+	'Highlander'
+];
+
+console.log(cars.map(cars => cars.length));
+//if no parameters then it will be ()=> { statements }
