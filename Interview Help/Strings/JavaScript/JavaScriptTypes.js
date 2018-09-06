23cd4a1512b08b5cfd5334e27df78076d6cefff1
Ken Wagner <ken_xwagner@hotmail.com>
2018-09-05 23:15:43 -0700

    More to know
    JavaScript of JavaScript Types.

---
+++ Interview Help/Strings/JavaScript/JavaScriptTypes.js
@@ -0,0 +1,19 @@
+//Important parts of JavaScript:
+
+var javaScriptTypes = [
+'Number',
+'String',
+'Boolean',
+'Symbol-ES2015',
+'Object',
+'Function',
+'Array',
+'Date',
+'RegExp',
+'null',
+'undefined'
+];
+
+console.log(javaScriptTypes.map(javaScriptTypes => javaScriptTypes.length));
+
+//if no parameters then it will be ()=> { statements }
