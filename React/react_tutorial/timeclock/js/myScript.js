3cd0d365fe76a38990cf35a472727e586ca80b9b
Ken Wagner <ken_xwagner@hotmail.com>
2018-09-24 23:35:42 -0700

    Changes

--- React/react_tutorial/timeclock/js/myScript.js
+++ React/react_tutorial/timeclock/js/myScript.js
@@ -1,37 +1,19 @@
 class Clock extends React.Component {
-  constructor() {
-    super();
-    let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    this.state = {
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
-    };
-  }
-  
-  updateClock(self) {
-    let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    self.setState({
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
-    });
-  }
-  
   render() {
     return (
-      <div>{this.state.hours}:{this.state.minutes}:{this.state.seconds}</div>
+      <div>
+        <h1> What time is it?</h1>
+        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        </div>
     );
   }
-  
-  componentDidMount() {
-    setInterval(this.updateClock, 1000, this);
-  }
 }
-ReactDOM.render(<Clock />, document.getElementById('app'));
+
+function tick() {
+  ReactDOM.render(
+    <Clock date={new Date()} />,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
