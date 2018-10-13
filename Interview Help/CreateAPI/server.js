94b62c0912046b9d8e7a9ece6a6c2042fb223e05
Ken Wagner <ken_xwagner@hotmail.com>
2018-10-13 14:26:46 -0700

    More to teach
    Create API with Express and Mongo

---
+++ Interview Help/CreateAPI/server.js
@@ -0,0 +1,7 @@
+// server.js
+var app = require('./app');
+var port = process.env.PORT || 3000;
+
+var server = app.listen(port, function() {
+	console.log('Express server listening on port ' + port);
+});
