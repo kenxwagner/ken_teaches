84e5037b851a4bcdd3cc267deeab6c3fef2a29f0
Ken Wagner <ken_xwagner@hotmail.com>
2018-11-05 23:27:24 -0800

    Updates

--- Interview Help/StockMarket/stockMarket.js
+++ Interview Help/StockMarket/stockMarket.js
@@ -1,10 +1,10 @@
 //array of given stock prices
-let stockArray = [12,11,15,14,13,11,10,9,12,15];
+let stockArray = [12,11,15,14,13,11,10,9,12,8];
 function stockMarketProfit() {
     
     let priceMax = 0;
     let priceMin = 0;
-    let profitMargin = priceMax priceMin;
+    let profitMargin = priceMax - priceMin;
 
     for (let arrayIndex = 0; i < stockArray.length; arrayIndex++) {
         if (stockArray[arrayIndex] > priceMax) {
@@ -16,4 +16,16 @@ function stockMarketProfit() {
 
     }
     return profitMargin;
+}
+stockMarketProfit(stockArray);
+
+
+// does not account for buying low BEFORE Selling high.
+
+
+function stockMarketProfit() {
+	let minIndex = 0;
+	let maxIndex = 1;
+	let priceMin = 0;
+	let profitMargin = 0;
 }
