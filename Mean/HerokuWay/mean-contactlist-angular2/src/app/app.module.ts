a9b10163efd2890931346bff11143ecc6f5e61fa
Ken Wagner <ken_xwagner@hotmail.com>
2018-11-16 23:49:58 -0800

    mean stack
    making a mean stack

---
+++ Mean/HerokuWay/mean-contactlist-angular2/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactDetailsComponent,
+    ContactListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
