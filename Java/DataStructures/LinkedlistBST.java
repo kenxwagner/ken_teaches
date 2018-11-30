4fdcd989beda8c37d8998311fa3317b8aded76ec
Ken Wagner <ken_xwagner@hotmail.com>
2018-11-29 23:17:47 -0800

    Update

--- Java/DataStructures/LinkedlistBST.java
+++ Java/DataStructures/LinkedlistBST.java
@@ -1,5 +1,30 @@
 class LinkedList {
-	static LNode head;
-	class LNode
-	
-}
+/* head node of link list */
+    static LNode head; 
+      
+    /* Link list Node */
+    class LNode  
+    { 
+        int data; 
+        LNode next, prev; 
+  
+        LNode(int d)  
+        { 
+            data = d; 
+            next = prev = null; 
+        } 
+    } 
+      
+    /* A Binary Tree Node */
+    class TNode  
+    { 
+        int data; 
+        TNode left, right; 
+  
+        TNode(int d)  
+        { 
+            data = d; 
+            left = right = null; 
+        } 
+    } 
+  
