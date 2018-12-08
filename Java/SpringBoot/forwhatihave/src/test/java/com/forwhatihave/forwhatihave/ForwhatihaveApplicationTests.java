7419bc6a9dc0486640d7ce71502e9eb9e45344c4
Ken Wagner <kenwagner@hotmail.com>
2018-12-07 19:20:03 -0800

    Created Spring app

---
+++ Java/SpringBoot/forwhatihave/src/test/java/com/forwhatihave/forwhatihave/ForwhatihaveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.forwhatihave.forwhatihave;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ForwhatihaveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
