d0f2d08634891d9856c71cb257904f412b1af326
Ken Wagner <ken_xwagner@hotmail.com>
2019-01-01 23:55:20 -0800

    removed old files
    removed unwanted files which  caused vulnerability issues.

--- React/react_tutorial/create-react-app/simple-component-library/src/index.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(<div>Welcome to the world</div>, document.getElementById("root"));
--- React/react_tutorial/create-react-app/simple-component-library/src/lib/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import "./TextInput.css";
-
-const TextInput = ({ type = "text", label, value, onChange}) => (
-	<div className="simple-form-group">
-	
-	)
-
