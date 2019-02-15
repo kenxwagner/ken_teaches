3dbc954e609dffad3c6cef441d26304e1e3fa03e
Ken Wagner <ken_xwagner@hotmail.com>
2019-02-14 23:31:06 -0800

    new piec

---
+++ React/pieces/index.js
@@ -0,0 +1,18 @@
+class Square extends React.Component {
+	render() {
+		return {
+			<button className="square">
+			{}
+			</button>
+		);
+	}
+}
+class Board extends React.Component {
+	renderSquare(i) {
+		return <Square />;
+	}
+	render() {
+	//finish here.
+	}
+	
+}
