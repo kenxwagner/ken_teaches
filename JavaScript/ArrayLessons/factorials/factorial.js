8516fe5d3fde554286db3ca1ce2ab1ecd6c816d5
Ken Wagner <kenwagner@hotmail.com>
2019-04-09 23:03:40 -0700

    Update factorial.js
    copy and paste fix

--- JavaScript/ArrayLessons/factorials/factorial.js
+++ JavaScript/ArrayLessons/factorials/factorial.js
@@ -1,9 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset=utf-8 />
-<title>Calculate the factorial of a number.</title>
-</head>
-<body>
-</body>
-</html>
+function factorial(x)
+{
+	if (x === 0)
+	{ return 1;
+	}
+	return x * factorial(x-1);
+	}
+	console.log(factorial(5));
