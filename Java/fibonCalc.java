b78b8159d277f733789ea598e70db253f7b46a36
Ken Wagner <ken_xwagner@hotmail.com>
2019-04-26 23:59:44 -0700

    Create fibonCalc

---
+++ Java/fibonCalc.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class FibonacciCalculator {
+	
+	public static void main(String args[]) {
+		
+		//input
+		System.out.println("Please enter a number to stop near for our Fibonacci series: ");
+		int number = new Scanner(System.in).nextInt();
+		
+		System.out.println("Fibonacci series up to " + number + " numbers: ");
+		//print number series
+		for(int i = 1;)
+		
+	}
+}
