a3d16ee584f48d289bac0bb97bc466095feb3623
Ken Wagner <ken_xwagner@hotmail.com>
2019-05-03 23:36:47 -0700

    update

--- JavaScript/TodaysDateSwitch.js
+++ JavaScript/TodaysDateSwitch.js
@@ -36,3 +36,43 @@ document.getElementById("todayis").innerHTML = "Today is " + day;
 
 </body>
 </html>
+<script>
+function getLetter(s) {
+    let letter;
+    // Write your code here
+    switch (true) {
+        case 'aeiou'.includes(s[0]):
+            letter = 'A';
+            break;
+        case 'bcdfg'.includes(s[0]):
+            letter = 'B';
+            break;
+        case 'hjklm'.includes(s[0]):
+            letter = 'C';
+            break;
+        case 'npqrstvwxyz'.includes(s[0]):
+            letter = 'D';
+            break;
+			//or
+		
+		case 'aeiou'.includes(s[0]):
+		   letter = 'A';
+			break;
+		case 'bcdfg'.includes(s[0]):
+			letter = 'B';
+			break;
+		case 'hjklm'.includes(s[0]):
+			letter = 'C';
+			break;
+		default:
+			letter = 'D';
+			break;
+			
+    }
+    return letter;
+}
+
+
+   
+		
+		</script>
