24d63ee63917a9e3602b2a2223d165d04f29d2ab
Ken Wagner <kenwagner@hotmail.com>
2019-05-29 22:04:27 -0700

    Update index.js
    passing doen two props from Board to Square, which are value and onClick.

--- ReactGame/my-game/src/index.js
+++ ReactGame/my-game/src/index.js
@@ -29,7 +29,12 @@ class Board extends React.Component {
 	  }
 
 	renderSquare(i) {
-		return <Square value={this.state.squares[i]}/>;
+		return (
+			 <Square 
+			 value={this.state.squares[i]}
+			 onClick={() => this.handleClick(i)}
+			 />
+		);
 	}
 	
 	render() {
