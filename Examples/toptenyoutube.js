2c63fe5554d07e9f0de75a31970c475d20fe5571
Ken Wagner <kenwagner@hotmail.com>
2019-05-30 20:28:46 -0700

    Create new component

---
+++ Examples/toptenyoutube.js
@@ -0,0 +1,26 @@
+class TopTenYoutube extends React.Component {
+  render() {
+    return (
+      <div className="TopTenYoutube-list">
+        <h1>Top Ten Youtube List for {this.props.name}</h1>
+        <ul>
+			<li>	Despacito	</li>
+			<li>	Shape of You	</li>
+			<li>	See You Again	</li>
+			<li>	Masha and the Bear – Recipe for Disaster	</li>
+			<li>	Uptown Funk	</li>
+			<li>	Gangnam Style	</li>
+			<li>	Sorry	</li>
+			<li>	Sugar	</li>
+			<li>	Baby Shark Dance	</li>
+			<li>	Roar	</li>
+
+        </ul>
+      </div>
+    );
+  }
+}
+
+// Example usage: <TopTenYoutube name="Shake It Off" />
+
+
