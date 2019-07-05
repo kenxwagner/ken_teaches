803b1e110c682555d82a91404a7ebbf30804fcfd
Ken Wagner <kenwagner@hotmail.com>
2019-07-04 22:14:35 -0700

    Create form.ts

---
+++ JavaScript/Angular/form1.js
@@ -0,0 +1,11 @@
+//this is a reactive form
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    // other imports ...
+    ReactiveFormsModule
+  ],
+})
+export class AppModule { }
