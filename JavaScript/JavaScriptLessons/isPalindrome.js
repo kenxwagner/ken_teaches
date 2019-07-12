47777fc79d0c796c2e4ea493e2415bde222e1a22
Ken Wagner <kenwagner@hotmail.com>
2019-07-11 22:19:02 -0700

    New files

---
+++ JavaScript/JavaScriptLessons/isPalindrome.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8" />
+</head>
+<body>
+<script type="text/javascript">
+	function check_Palindrome()
+	{
+		var revStr = "";
+		var str = document.getElementById("str").value;
+		var i = str.length;
+		
+		for(var j = i; j >= 0; j--)
+		{
+			revStr = revStr + str.charAt(j);
+		}
+		
+</script>
+</body>
+</html>
