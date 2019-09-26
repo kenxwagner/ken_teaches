172339a0d268b9062c81625d49bdf544ccb1be9f
Ken Wagner <kenwagner@hotmail.com>
2019-09-25 21:54:07 -0700

    Update form1.js

--- React/forms/forms1.js
+++ React/forms/forms1.js
@@ -3,3 +3,21 @@ class NameForm extends Component {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+  
+  handleSubmit(e) {
+    alert('The value is: ' + this.input.value);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" ref={(input) => this.input = input} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
