5341c686c56471f6b298a0a4627971ca2012e2f9
Ken Wagner <kenwagner@hotmail.com>
2019-09-27 17:56:58 -0700

    create file

---
+++ React/forms/container.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';  
+
+/* Import Components */
+import CheckBox from '../components/CheckBox';  
+import Input from '../components/Input';  
+import TextArea from '../components/TextArea';  
+import Select from '../components/Select';
+import Button from '../components/Button'
+
+class FormContainer extends Component {  
+  constructor(props) {
+    super(props);
