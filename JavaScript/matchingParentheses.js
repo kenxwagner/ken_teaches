e492440e18f3cedbbb9b9ea891fb09e5b36930a5
Ken Wagner <kenwagner@hotmail.com>
2019-10-03 19:51:40 -0700

    new file

---
+++ JavaScript/matchingParentheses.js
@@ -0,0 +1,27 @@
+let isMatchingBrackets = function (str) {
+    let stack = [];
+    let map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+	
+	for (let i = 0; i < str.length; i++) {
+
+        // If character is an opening brace add it to a stack
+        if (str[i] === '(' || str[i] === '{' || str[i] === '[' ) {
+            stack.push(str[i]);
+        }
+        //  If that character is a closing brace, pop from the stack, which will also reduce the length of the stack each time a closing bracket is encountered.
+        else {
+            let last = stack.pop();
+
+            //If the popped element from the stack, which is the last opening brace doesn’t match the corresponding closing brace in the map, then return false
+            if (str[i] !== map[last]) {return false};
+        }
+    }
+    // By the completion of the for loop after checking all the brackets of the str, at the end, if the stack is not empty then fail
+        if (stack.length !== 0) {return false};
+
+    return true;
+}
