f1011abdd5f5f69b7ea84754a0cab5cf2dbf1626
Ken Wagner <kenwagner@hotmail.com>
2019-10-07 11:17:12 -0700

    Create HashMap2sums.java

---
+++ Java/HashMap/HashMap2sums.java
@@ -0,0 +1,7 @@
+//Given an array of integers, return indices of the two numbers such that they add up to a specific target
+
+public int[] twoSum(int[] nums, int target) {
+
+	Maps<Integer, Integer> map = new HashMaps<>();
+
+}
