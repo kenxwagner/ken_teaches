16f493043172ceff2196f251df6abf407907436e
Ken Wagner <kenwagner@hotmail.com>
2019-10-08 22:03:46 -0700

    Created fibonaccijs

---
+++ JavaScript/FibonacciJS.js
@@ -0,0 +1,15 @@
+//fibonacci iterative
+function fib(n){
+	const result = [0, 1];
+	
+	for (let i = 2; i <=n;, i++) {
+	const a = result[i-1];
+	const b = result[i-2];
+	
+	result.push(a+b);
+	}
+	return result[n];
+}
+
+module.exports = fib;
+
