83a4244ae314390774d4ffba680f0198100ff2ca
Ken Wagner <kenwagner@hotmail.com>
2020-01-03 20:00:12 -0800

    Creat app.jsx

---
+++ JavaScript/React/@random/app.jsx
@@ -0,0 +1,25 @@
+Import React from ‘react’;
+
+Class App extends React.Component {
+
+                Render() {
+
+                Return (
+
+                                <div>
+
+                                <h1>Header</h1>
+
+                                <h2>Content</h2>
+
+                                <p>More Content such as paragraphs.</p>
+
+                                </div>
+
+                                );
+
+                }
+
+}
+
+Export default App;
