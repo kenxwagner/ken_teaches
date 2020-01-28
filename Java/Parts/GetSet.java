d717c989613ce4af1bd1607211be23debb0e0371
Ken Wagner <ken_xwagner@hotmail.com>
2020-01-27 23:54:32 -0800

    Create file

---
+++ Java/Parts/GetSet.java
@@ -0,0 +1,13 @@
+public class Vehicle {
+  private String color;
+
+  // Getter
+  public String getColor() {
+    return color;
+  }
+
+ // Setter
+  public void setColor(String a) {
+    this.color = a;
+  }
+}
