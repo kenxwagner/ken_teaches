1e9401c421764b9d9c988de49887b747684a14c0
Ken Wagner <kenwagner@hotmail.com>
2020-02-02 18:29:24 -0800

    New file

---
+++ Java/Parts/Dictionary1.java
@@ -0,0 +1,18 @@
+import java.util.*; 
+public class New_Class 
+{ 
+    public static void main(String[] args) 
+    { 
+  
+        // Initialize a Dictionary 
+        Dictionary fun = new Hashtable(); 
+  
+        // put() method 
+        fun.put("123", "Gitter"); 
+        fun.put("456", "Done"); 
+  
+        
+        System.out.println("\nHere is the size of Dictionary : " + fun.size()); 
+  
+    } 
+} 
