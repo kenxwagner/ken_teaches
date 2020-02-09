168c9dabf9a5879f8230c24a6f01748e59c81498
Ken Wagner <kenwagner@hotmail.com>
2020-02-08 17:05:12 -0800

    new file

---
+++ Java/Parts/staticvspublic.java
@@ -0,0 +1,20 @@
+public class MyClass {
+  // Static method
+  static void myStaticMethod() {
+    System.out.println("Static methods can be called without creating objects");
+  }
+
+  // Public method
+  public void myPublicMethod() {
+    System.out.println("Public methods must be called by creating objects");
+  }
+
+  // Main method
+  public static void main(String[ ] args) {
+    myStaticMethod(); // Call the static method
+    // myPublicMethod(); This would output an error
+
+    MyClass myObj = new MyClass(); // Create an object of MyClass
+    myObj.myPublicMethod(); // Call the public method
+  }
+}
