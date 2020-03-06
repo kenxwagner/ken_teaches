4e679986d4df318bd5b51a684a0d63c305d2b44a
Ken Wagner <kenwagner@hotmail.com>
2020-03-05 19:58:27 -0800

    New file

--- JavaScript/CodeChallenge/HowManyNumbersAreSmaller.js
+++ JavaScript/CodeChallenge/HowManyNumbersAreSmaller.js
@@ -3,8 +3,8 @@
  * @return {number[]}
  */
 var smallerNumbersThanCurrent = function(nums) {
-    const sorted = Array.from(nums).sort((num1, num2) => num2 - num1);
-    const map = new Map(sorted.map((num, index) => [num, nums.length - index - 1]));
+    const sortedArray = Array.from(nums).sort((num1, num2) => num2 - num1);
+    const map = new Map(sortedArray.map((num, index) => [num, nums.length - index - 1]));
     return nums.map(num => map.get(num));
     
 }
