b281ef2b5b23929b85f0787d1f16dd3484f818c9
Ken Wagner <kenwagner@hotmail.com>
2020-03-15 21:18:49 -0700

    Create addTwoNumbers.java

---
+++ Code Challenge/addTwoNumbers.java
@@ -0,0 +1,9 @@
+public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode dummyHead = new ListNode(0);
+    ListNode p = l1, q = 12, curr = dummyHead;
+    int carry = 0;
+    while(p != null || q != null) {
+
+        int x = (p != null) ? p.val : 0;
+    }
+}
