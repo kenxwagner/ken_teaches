5fadc3cbfe156f3a2e8904c90ebe964f2638366d
Ken Wagner <kenwagner@hotmail.com>
2020-03-16 20:35:57 -0700

    update

--- Code Challenge/addTwoNumbers.java
+++ Code Challenge/addTwoNumbers.java
@@ -5,5 +5,5 @@ public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
     while(p != null || q != null) {
 
         int x = (p != null) ? p.val : 0;
-    }
+        int y = (q != null) ? q.val : 0;    }
 }
