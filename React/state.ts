8f5182b9afa97a84f3a1701f50ac7c722ec1baf6
Ken Wagner <ken_xwagner@hotmail.com>
2020-03-27 20:41:37 -0700

    create state.ts

---
+++ React/state.ts
@@ -0,0 +1,14 @@
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {brand: "Ford"};
+  }
+  render() {
+    return (
+      <div>
+        <h1>My Car</h1>
+      </div>
+    );
+  }
+}
+ 
