fdffe40a2180e3ea44e8d32f45e1815323d7d377
Ken Wagner <ken_xwagner@hotmail.com>
2020-08-15 20:17:13 -0700

    Create file

---
+++ Rest/other.ts
@@ -0,0 +1,14 @@
+class Other extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {brand: "Blah"};
+  }
+  render() {
+    return (
+      <div>
+        <h1>My Other</h1>
+      </div>
+    );
+  }
+}
+ 
