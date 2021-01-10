17784bf4f9fdb25fa94c407378b999d42b9dcae9
Ken Wagner <kenwagner@hotmail.com>
2021-01-09 17:41:16 -0800

    updates

--- Code Challenge/SimpleDataStructures/ArraySlicing.js
+++ Code Challenge/SimpleDataStructures/ArraySlicing.js
@@ -5,3 +5,10 @@ function cutItOut(myArray) {
 	myArray.slice(startIndex, endIndex); 
 // or  myArray.slice(startIndex);
 }
+
+//slicing allocates a new array
+//and you are copying from original to the new array
+
+//explained further by saving it as a variable
+
+const tailOfArray = myArray.slice(1);
